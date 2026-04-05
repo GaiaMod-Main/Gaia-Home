aee2cdda55112eee046dabeff534e11d66b22d07
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-05 18:28:02 -0400

    hi

--- src/components/whats-new/whats-new-data.js
+++ src/components/whats-new/whats-new-data.js
@@ -1,11 +1,12 @@
 import blockFonts from './blockfonts.gif'
+import firstDay from './firstday.png'
 
 export default {
-    message: "You can now change the fonts of blocks and dropdowns with an addon!",
-    author: "By Gabs The Mr Incredible Maker (GabsTheCuriousKid)",
-    picture: blockFonts,
-    avatarUrl: "https://avatars.githubusercontent.com/u/140965985?v=4&size=40",
-    secondMessage: "Sorry for the awful quality :(",
+    message: "A web was born!",
+    author: "By Gaiawindwave90",
+    picture: firstDay,
+    avatarUrl: "https://avatars.githubusercontent.com/u/4597024?v=4&size=40",
+    secondMessage: "Sorry because I was having a meltdown over something...",
     pictureData: {
         width: "450px",
         height: "200px"
