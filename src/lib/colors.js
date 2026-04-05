101ee22dcc6e8d5a424d89560a0569cb5a026398
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-05 17:44:14 -0400

    Update colors.js

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     const root = document.documentElement;
     root.style.setProperty("--motion-primary", "hsla(240, 65%, 50%, 1)")
-    root.style.setProperty("--motion-primary-menu", "#hsla(240, 65%, 28%, 1)")
+    root.style.setProperty("--motion-primary-menu", "hsla(240, 65%, 28%, 1)")
     root.style.setProperty("--motion-primary-dark", "hsla(240, 65%, 40%, 1)")
 })
