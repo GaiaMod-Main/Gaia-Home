3d0a4c562c27b57a0759c117b517e0558d33fd90
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-25 03:11:18 -0400

    Wahoo!

--- src/lib/example-cards/index.js
+++ src/lib/example-cards/index.js
@@ -1,9 +1,12 @@
-import DinoClickerThumb from './DinoClickerThumb.png';
-import SwearDetectorThumb from './SwearDetectorThumb.png';
+import WhackVioThumb from './DinoClickerThumb.png';
+import DominoThumb from './SwearDetectorThumb.png';
+import MiniGaiaClickerThumb from './SwearDetectorThumb.png';
+import ChatGPTThumb from './SwearDetectorThumb.png';
+import ILoveYouThumb from './SwearDetectorThumb.png';
 
 const exampleCards = [
     {
-        name: "Dino Clicker",
+        name: "Whack-A-Viorel",
         iconURL: DinoClickerThumb,
         href: "https://dinosaurmod.github.io/?project_url=https://raw.githubusercontent.com/Dinosaurmod/examples/main/src/dino-files/DinoClicker.dino"
     },
